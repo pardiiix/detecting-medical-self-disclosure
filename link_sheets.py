7ba0c5488978a1b2918befac9f6141f04d801d30
Pardiiix <pardis.ranjbar@gmail.com>
2020-03-23 14:56:19 -0500

    created the dataframe

--- link_sheets.py
+++ link_sheets.py
@@ -3,20 +3,24 @@ from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 import pandas as pd
 
+#Link to the Google Excel Sheet
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("SD_Annotation").sheet1
 
 data = sheet.get_all_records()
-# pprint(data)
 
+#Create dataframe from all the data in Excel Sheet
 whole_df = pd.DataFrame.from_dict(data)
-# Comments = whole_df["Comments"]
-# Labels = whole_df["Labels"]
-# print(Comments)
 
-#create dataframe with two columns comments and labels
-df = pd.DataFrame(columns = ['Comments, Labels'])
-df['Comments'] = whole_df["Comments"]
-df["Labels"] = whole_df["Labels"]
+'''
+Create dataframe with three columns Exact_Comments, Comments and Labels.
+Exact_Comments are the raw comments without any preprocessing
+Comments are to be preprocessed
+'''
+def create_dataframe():
+	df = pd.DataFrame(columns = ['Exact_Comments','Comments', 'Labels'])
+	df['Exact_Comments'] = df['Comments'] = whole_df["Comments"]
+	df["Labels"] = whole_df["Labels"]
+	return df
